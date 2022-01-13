3eeca18f83b8202af1e4f3f6525a431558ea2e7a
eunbin park <eunbinpark@eunbinui-MacBookPro.local>
2022-01-14 02:05:48 +0900

    Feat/client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,20 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Templatepage from "./pages/Templatepage";
 import Mypage from "./pages/Mypage";
+import PurchaseDetail from "./pages/purchaseDetail";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<div className="w-3/5 m-auto mb-10">
-				<Routes>
-					<Route path="/" element={<Templatepage />} />
-					<Route path="/mypage" element={<Mypage />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className="w-3/5 m-auto mb-10 md:min-w-full">
+        <Routes>
+          <Route path="/" element={<Templatepage />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/purchase" element={<PurchaseDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- server/controller/purchase/postPurchase.js
+++ server/controller/purchase/postPurchase.js
@@ -3,64 +3,65 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 
 module.exports = async (req, res) => {
-	const handleToken = req.cookies.handleToken;
-	const {
-		responser,
-		productName,
-		productInfo,
-		quantity,
-		price,
-		totalPrice,
-		reason,
-	} = req.body;
+  console.log(req);
+  const handleToken = req.cookies.handleToken;
+  const {
+    responser,
+    productName,
+    productInfo,
+    quantity,
+    price,
+    totalPrice,
+    reason,
+  } = req.body;
 
-	if (!handleToken) {
-		return res.status(401).json({ mesage: "로그인을 해주세요" });
-	}
+  if (!handleToken) {
+    return res.status(401).json({ mesage: "로그인을 해주세요" });
+  }
 
-	try {
-		const userInfo = await jwt.verify(handleToken, process.env.TOKEN);
-		const requesterInfo = await users.findOne({ where: { id: responser } });
-		if (
-			responser &&
-			productName &&
-			productInfo &&
-			quantity &&
-			price &&
-			totalPrice &&
-			reason
-		) {
-			let newPurchase = await purchaseAgreement.create({
-				requester: userInfo.id,
-				responser,
-				title: "비품 신청",
-				productName,
-				productInfo,
-				quantity,
-				price,
-				totalPrice,
-				reason,
-				result: null,
-			});
-			console.log(newPurchase);
-			res.status(200).json({ message: "비품동의서 저장에 성공하였습니다" });
+  try {
+    const userInfo = await jwt.verify(handleToken, process.env.TOKEN);
+    const requesterInfo = await users.findOne({ where: { id: responser } });
+    if (
+      responser &&
+      productName &&
+      productInfo &&
+      quantity &&
+      price &&
+      totalPrice &&
+      reason
+    ) {
+      let newPurchase = await purchaseAgreement.create({
+        requester: userInfo.id,
+        responser,
+        title: "비품 신청",
+        productName,
+        productInfo,
+        quantity,
+        price,
+        totalPrice,
+        reason,
+        result: null,
+      });
+      console.log(newPurchase);
+      res.status(200).json({ message: "비품동의서 저장에 성공하였습니다" });
 
-			try {
-				await axios.post(`${process.env.HANDLE_API_URL}/msg/aligo`, {
-					receiver: requesterInfo.phoneNumber,
-					msg: `확인이 필요한 비품동의서가 있습니다.
+      try {
+        await axios.post(`${process.env.HANDLE_API_URL}/msg/aligo`, {
+          receiver: requesterInfo.phoneNumber,
+          msg: `확인이 필요한 비품동의서가 있습니다.
 				  https://works.handle.market/purchase${newPurchase.id}
 				  `,
-				});
-			} catch (err) {
-				res.status(500).send("알리고 에러");
-				console.log(err);
-			}
-		} else {
-			res.status(400).json({ message: "parameter가 불충분합니다" });
-		}
-	} catch (err) {
-		res.status(500).send("서버에러");
-		console.log(err);
-	}
+        });
+      } catch (err) {
+        res.status(500).send("알리고 에러");
+        console.log(err);
+      }
+    } else {
+      res.status(400).json({ message: "parameter가 불충분합니다" });
+    }
+  } catch (err) {
+    res.status(500).send("서버에러");
+    console.log(err);
+  }
 };
