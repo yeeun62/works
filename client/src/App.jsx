04d0124ea773ac5a6fcff141ef1978c76ba2870d
eunbin park <eunbinpark@eunbinui-MacBookPro.local>
2022-01-14 10:43:49 +0900

    merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,33 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Templatepage from "./pages/Templatepage";
-import Mypage from "./pages/Mypage";
 import PurchaseDetail from "./pages/PurchaseDetail";
+import Mypage from "./pages/Mypage";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(async () => {
+    try {
+      let user = await axios.get(
+        `${process.env.REACT_APP_TEMPLATE_API_URL}/user/userInfo`,
+        {
+          withCredentials: true,
+        }
+      );
+      setUserInfo(user.data.userInfo);
+    } catch {}
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="w-3/5 m-auto mb-10 md:min-w-full">
         <Routes>
-          <Route path="/" element={<Templatepage />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/purchase" element={<PurchaseDetail />} />
+          <Route path="/" element={<Templatepage userInfo={userInfo} />} />
+          <Route path="/mypage" element={<Mypage userInfo={userInfo} />} />
+          <Route path="/purchase/:id" element={<PurchaseDetail />} />
         </Routes>
       </div>
     </BrowserRouter>
