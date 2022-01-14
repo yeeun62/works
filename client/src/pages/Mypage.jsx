5d5d392e4eb438eefb03e39792eacfde855a32da
yeeun62 <byebye62@naver.com>
2022-01-14 15:41:56 +0900

    [Add]

--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -5,36 +5,30 @@ import DocList from "../components/DocList";
 import "../style/mypage.css";
 
 const Mypage = ({ userInfo }) => {
-  const [tabMenu, setTabMenu] = useState(false);
-  const [list, setList] = useState({ myRequest: [], myResponse: [] });
-  
-  const tabHandler = (b) => {
-    setTabMenu(b);
-  };
+	const [tabMenu, setTabMenu] = useState(false);
+	const [list, setList] = useState({ myRequestList: [], myResponserList: [] });
 
-  // useEffect(() => {
-  //   console.log(document.cookie);
-  //   let cookie = document.cookie;
-  //   try {
-  //     axios
-  //       .get(`${process.env.REACT_APP_TEMPLATE_API_URL}/user/doc`, {
-  //         headers: { cookie: cookie },
-  //       })
-  //       .then((res) => {
-  //         if (res.status === 200) {
-  //           setList({
-  //             myRequest: res.data.myRequest,
-  //             myResponse: res.data.myResponser,
-  //           });
-  //           console.log(res.data);
-  //         } else {
-  //           console.log(res);
-  //         }
-  //       });
-  //   } catch (err) {
-  //     console.log("캐치에러", err);
-  //   }
-  // }, [list]);
+	const tabHandler = () => {
+		setTabMenu(!tabMenu);
+	};
+
+	useEffect(() => {
+		console.log(Boolean(list));
+		axios
+			.get(`${process.env.REACT_APP_TEMPLATE_API_URL}/user/doc`, {
+				withCredentials: true,
+			})
+			.then((res) => {
+				console.log(res);
+				if (res.status === 200) {
+					setList({
+						myRequestList: res.data.myRequestList,
+						myResponserList: res.data.myResponserList,
+					});
+					console.log(res.data);
+				}
+			});
+	}, []);
 
 	return (
 		<>
@@ -43,7 +37,7 @@ const Mypage = ({ userInfo }) => {
 				<div className="tabButton">
 					<button
 						className={tabMenu ? "myRequest btn trueTab" : "myRequest btn"}
-						onClick={() => tabHandler(true)}
+						onClick={tabHandler}
 					>
 						보낸 요청
 					</button>
@@ -51,7 +45,7 @@ const Mypage = ({ userInfo }) => {
 						className={
 							tabMenu ? "receivedRequest btn" : "receivedRequest btn trueTab"
 						}
-						onClick={() => tabHandler(false)}
+						onClick={tabHandler}
 					>
 						받은 요청
 					</button>
@@ -59,9 +53,8 @@ const Mypage = ({ userInfo }) => {
 				<div className="tabMenu">
 					{
 						<DocList
-							list={tabMenu ? list.myRequest : list.myResponse}
-							userInfo={userInfo}
-						></DocList>
+							list={tabMenu ? list.myRequestList : list.myResponserList}
+						/>
 					}
 				</div>
 			</div>
