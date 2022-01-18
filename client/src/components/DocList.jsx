48e00ea2b52166a3d6bb5779dccb322e69c8aa5a
eunbin park <eunbinpark@eunbinui-MacBookPro.local>
2022-01-18 22:27:10 +0900

    update

--- client/src/components/DocList.jsx
+++ client/src/components/DocList.jsx
@@ -1,39 +1,83 @@
 import { Link } from "react-router-dom";
 
-export default function DocList({ list, tabMenu }) {
-	return (
-		<div className="RequestContainer">
-			<div className="header">
-				{tabMenu ? (
-					<p className="writer">요청자</p>
-				) : (
-					<p className="writer">작성자</p>
-				)}
-				<p className="title">제목</p>
-				<p className="result">결과</p>
-				<p className="date">요청일</p>
-			</div>
-			<ul>
-				{list.length ? (
-					list.map((el) => {
-						let purchaseId = `/purchase/${el.purchaseId}`;
-						return (
-							<Link key={el.purchaseId} to={purchaseId}>
-								<li className="requestList">
-									<p className="requester">{el.requester}</p>
-									<p className="title">{el.title}</p>
-									<p className="result">
-										{el.result ? "승인" : el.result == null ? "대기중" : "거절"}
-									</p>
-									<p className="date">{el.createdAt.slice(0, 10)}</p>
-								</li>
-							</Link>
-						);
-					})
-				) : (
-					<p className="noRequest"> 요청이 없습니다 🙌</p>
-				)}
-			</ul>
-		</div>
-	);
+export default function DocList({ list, tabMenu, filter }) {
+  const listArray = (filter, list) => {
+    if (filter === "all") {
+      return list.map((el) => {
+        let purchaseId = `/purchase/${el.purchaseId}`;
+        return (
+          <Link key={el.purchaseId} to={purchaseId}>
+            <li className="requestList">
+              <p className="requester">{el.requester}</p>
+              <p className="title">{el.title}</p>
+              <p className="result">
+                {el.result ? "승인" : el.result == null ? "대기중" : "거절"}
+              </p>
+              <p className="date">{el.createdAt.slice(0, 10)}</p>
+            </li>
+          </Link>
+        );
+      });
+    } else {
+      let filtered = list.filter((el) => {
+        return el.result === filter;
+      });
+
+      return filtered.map((el) => {
+        let purchaseId = `/purchase/${el.purchaseId}`;
+        return (
+          <Link key={el.purchaseId} to={purchaseId}>
+            <li className="requestList">
+              <p className="requester">{el.requester}</p>
+              <p className="title">{el.title}</p>
+              <p className="result">
+                {el.result ? "승인" : el.result == null ? "대기중" : "거절"}
+              </p>
+              <p className="date">{el.createdAt.slice(0, 10)}</p>
+            </li>
+          </Link>
+        );
+      });
+    }
+  };
+  console.log("그냥 리스트", list, "필터", filter);
+
+  console.log("리스트어레이", listArray(filter, list));
+
+  return (
+    <div className="RequestContainer">
+      <div className="header">
+        {tabMenu ? (
+          <p className="writer">요청자</p>
+        ) : (
+          <p className="writer">작성자</p>
+        )}
+        <p className="title">제목</p>
+        <p className="result">결과</p>
+        <p className="date">요청일</p>
+      </div>
+      <ul>
+        {list.length ? (
+          // list.map((el) => {
+          //   let purchaseId = `/purchase/${el.purchaseId}`;
+          //   return (
+          //     <Link key={el.purchaseId} to={purchaseId}>
+          //       <li className="requestList">
+          //         <p className="requester">{el.requester}</p>
+          //         <p className="title">{el.title}</p>
+          //         <p className="result">
+          //           {el.result ? "승인" : el.result == null ? "대기중" : "거절"}
+          //         </p>
+          //         <p className="date">{el.createdAt.slice(0, 10)}</p>
+          //       </li>
+          //     </Link>
+          //   );
+          // })
+          listArray(filter, list)
+        ) : (
+          <p className="noRequest"> 요청이 없습니다 🙌</p>
+        )}
+      </ul>
+    </div>
+  );
 }
--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -7,6 +7,7 @@ import "../style/mypage.css";
 const Mypage = () => {
   const [tabMenu, setTabMenu] = useState(false);
   const [list, setList] = useState({ myRequestList: [], myResponseList: [] });
+  const [filter, setFilter] = useState("");
 
   const tabHandler = (boolean) => {
     setTabMenu(boolean);
@@ -30,6 +31,15 @@ const Mypage = () => {
     <>
       <Header />
       <div className="myPageWrapper">
+        <select
+          className="filterDoc"
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="all">모든 문서</option>
+          <option value={null}>대기 중</option>
+          <option value={true}>승인</option>
+          <option value={false}>거절</option>
+        </select>
         <div className="tabButton">
           <button
             className={tabMenu ? "myRequest btn trueTab" : "myRequest btn"}
@@ -51,6 +61,7 @@ const Mypage = () => {
             <DocList
               tabMenu={tabMenu}
               list={tabMenu ? list.myRequestList : list.myResponseList}
+              filter={filter}
             />
           }
         </div>
