c4167a3ff4838c77a919203a397bc70a392a8c4c
yeeun62 <byebye62@naver.com>
2022-01-25 09:53:50 +0900

    [Add]

--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -5,23 +5,24 @@ import UserInfo from "../components/UserInfo";
 import axios from "axios";
 
 export default function MyPage() {
-  const [pwCheck, setPwCheck] = useState(false);
-  const pwCheckHandler = async (pw) => {
-    let postReq = await axios.post(
-      `${process.env.REACT_APP_TEMPLATE_API_URL}/user/pwcheck`,
-      { data: pw },
-      { withCredentials: true }
-    );
-    if (postReq.status === 200) setPwCheck(true);
-    console.log(postReq);
-  };
+	const [pwCheck, setPwCheck] = useState(false);
 
-  return (
-    <div>
-      <Header />
-      <div className="myWrapper">
-        {pwCheck ? <UserInfo /> : <PwCheck pwCheckHandler={pwCheckHandler} />}
-      </div>
-    </div>
-  );
+	const pwCheckHandler = async (pw) => {
+		let postReq = await axios.post(
+			`${process.env.REACT_APP_TEMPLATE_API_URL}/user/pwcheck`,
+			{ data: pw },
+			{ withCredentials: true }
+		);
+		if (postReq.status === 200) setPwCheck(true);
+		console.log(postReq);
+	};
+
+	return (
+		<div>
+			<Header />
+			<div className="myWrapper">
+				{pwCheck ? <UserInfo /> : <PwCheck pwCheckHandler={pwCheckHandler} />}
+			</div>
+		</div>
+	);
 }
