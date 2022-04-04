2c87b5a3f88aecc9aa1624d8d5e4f32436ae1f00
yeeun62 <byebye62@naver.com>
2022-04-04 15:19:18 +0900

    [Fix]

--- server/app.js
+++ server/app.js
@@ -11,20 +11,20 @@ const purchaseRouter = require("./router/purchase");
 app.use(cookieParser());
 app.use(express.json());
 app.use(
-	cors({
-		origin: true,
-		credentials: true,
-		methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
-	})
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+  })
 );
 
 app.use("/user", userRouter);
 app.use("/purchase", purchaseRouter);
 
 app.get("/", (req, res) => {
-	res.send("template 서버");
+  res.send("works 서버");
 });
 
-server.listen(80, () => console.log("template 서버 실행"));
+server.listen(80, () => console.log("works 서버 실행"));
 // sequelize.sync({ alter: true })
 // console.log("template 서버 실행")
--- server/controller/user/signin.js
+++ server/controller/user/signin.js
@@ -3,37 +3,37 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 module.exports = async (req, res) => {
-	const { email, password } = req.body;
-	const findUser = await handle_works_users.findOne({ where: { email } });
+  const { email, password } = req.body;
+  const findUser = await handle_works_users.findOne({ where: { email } });
 
-	try {
-		if (!findUser) {
-			res.status(401).json({ message: "일치하는 유저정보가 없습니다🥲" });
-		} else if (findUser) {
-			let passwordCheck = await bcrypt.compare(
-				password,
-				findUser.dataValues.password
-			);
-			if (!passwordCheck) {
-				return res
-					.status(401)
-					.json({ message: "비밀번호가 일치하지 않습니다🥲" });
-			} else if (passwordCheck) {
-				delete findUser.dataValues.password;
-				const userToken = await jwt.sign(
-					findUser.dataValues,
-					process.env.TOKEN
-				);
-				res
-					.status(200)
-					.cookie("handleToken", userToken, {
-						httpOnly: true,
-					})
-					.json({ message: "로그인에 성공하였습니다🥳" });
-			}
-		}
-	} catch (err) {
-		console.log("signin.js 서버에러", err);
-		res.status(500).json({ message: "서버에러 입니다🥲" });
-	}
+  try {
+    if (!findUser) {
+      res.status(401).json({ message: "일치하는 유저정보가 없습니다🥲" });
+    } else if (findUser) {
+      let passwordCheck = await bcrypt.compare(
+        password,
+        findUser.dataValues.password
+      );
+      if (!passwordCheck) {
+        return res
+          .status(401)
+          .json({ message: "비밀번호가 일치하지 않습니다🥲" });
+      } else if (passwordCheck) {
+        delete findUser.dataValues.password;
+        const userToken = await jwt.sign(
+          findUser.dataValues,
+          process.env.TOKEN
+        );
+        res
+          .status(200)
+          .cookie("handleToken", userToken, {
+            httpOnly: true,
+          })
+          .json({ message: "로그인에 성공하였습니다🥳" });
+      }
+    }
+  } catch (err) {
+    console.log("signin.js 서버에러", err);
+    res.status(500).json({ message: "서버에러 입니다🥲" });
+  }
 };
